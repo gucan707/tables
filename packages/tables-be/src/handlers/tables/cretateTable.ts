b2fee1db1939c2abf44947efa124f85239fe104f
gucan707 <1154984091@qq.com>
2023-03-14 18:54:01 +0800

    feat(be): 增加新表格 rows 初值

--- packages/tables-be/src/handlers/tables/cretateTable.ts
+++ packages/tables-be/src/handlers/tables/cretateTable.ts
@@ -2,6 +2,9 @@ import { IMiddleware } from "koa-router";
 import { ObjectId } from "mongodb";
 
 import {
+  DateFormatOptions,
+  NumberFormatDecimal,
+  NumberFormatPercent,
   ResCommon,
   ResCreateTable,
   Row,
@@ -10,19 +13,30 @@ import {
   TableHeads,
 } from "@tables/types";
 
-import { tables } from "../../db";
+import { rows, tables } from "../../db";
 import { checkToken } from "../../utils/checkToken";
+
+const INIITIAL_ROW_NUM = 3;
 
 export const createTable: IMiddleware = async (ctx) => {
   const userInfo = checkToken(ctx);
 
   const tableId = new ObjectId().toString();
   const initialTable = getInitialTable(userInfo._id, tableId);
-  await tables.insertOne(initialTable);
+
+  const initialRows: Row[] = [];
+  for (let i = 0; i < INIITIAL_ROW_NUM; i++) {
+    initialRows.push(getInitialTableRow(tableId, initialTable.heads));
+  }
+
+  await Promise.all([
+    tables.insertOne(initialTable),
+    rows.insertMany(initialRows),
+  ]);
 
   const res: ResCommon<ResCreateTable> = {
     status: 200,
-    data: { ...initialTable, body: [] },
+    data: { ...initialTable, body: initialRows },
     msg: "ok",
   };
 
@@ -53,13 +67,66 @@ function getInitialTableHeads(): TableHeads {
   return heads;
 }
 
-// const INIITIAL_ROW_NUM = 5;
-
-// function getInitialTableBody(tableId: string): Row[] {
-//   return Array.from<Row>({ length: INIITIAL_ROW_NUM }).map(() => {
-//     return {
-//       tableId,
-//       data: [],
-//     };
-//   });
-// }
+function getInitialTableRow(tableId: string, heads: TableHeads): Row {
+  const row: Row = {
+    _id: new ObjectId().toString(),
+    data: [],
+    tableId,
+  };
+  heads.forEach((head) => {
+    const common = {
+      _id: new ObjectId().toString(),
+      headId: head._id,
+      version: 0,
+    };
+    switch (head.type) {
+      case TableColumnTypes.Checkbox:
+        row.data.push({
+          ...common,
+          checked: false,
+          type: head.type,
+        });
+        break;
+      case TableColumnTypes.Date:
+        row.data.push({
+          ...common,
+          type: head.type,
+          date: -1,
+          format: DateFormatOptions.YMD,
+        });
+        break;
+      case TableColumnTypes.MultiSelect:
+        row.data.push({
+          type: head.type,
+          contents: [],
+          ...common,
+        });
+        break;
+      case TableColumnTypes.Number:
+        row.data.push({
+          type: head.type,
+          ...common,
+          content: 0,
+          decimal: NumberFormatDecimal.None,
+          percent: NumberFormatPercent.None,
+        });
+        break;
+      case TableColumnTypes.Select:
+        row.data.push({
+          type: head.type,
+          ...common,
+          content: "",
+        });
+        break;
+      case TableColumnTypes.Text:
+        row.data.push({
+          type: head.type,
+          ...common,
+          text: "",
+        });
+      default:
+        break;
+    }
+  });
+  return row;
+}
