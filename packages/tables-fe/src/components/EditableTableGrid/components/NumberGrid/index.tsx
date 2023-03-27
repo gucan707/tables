26611cabd3c8ed819a3ee27677a24f1ca553168e
gucan707 <1154984091@qq.com>
2023-03-27 15:36:28 +0800

    feat(fe): number grid 同步

--- packages/tables-fe/src/components/EditableTableGrid/components/NumberGrid/index.tsx
+++ packages/tables-fe/src/components/EditableTableGrid/components/NumberGrid/index.tsx
@@ -1,13 +1,15 @@
 import "./index.less";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { InputNumber } from "@arco-design/web-react";
-import { NumberType } from "@tables/types";
+import { NumberType, TableColumnTypes } from "@tables/types";
 
 import { CommonGridProps } from "../..";
+import { putGridContent } from "../../../../http/table/putGridContent";
 import { changeActiveGridId } from "../../../../redux/activeGridSlice";
-import { useAppDispatch } from "../../../../redux/store";
+import { useAppDispatch, useAppSelector } from "../../../../redux/store";
 import { getFormattedNumber } from "../../../../utils/getFormattedNumber";
 
 export type NumberGridProps = {
@@ -19,7 +21,23 @@ export const NumberGrid: FC<NumberGridProps> = (props) => {
   const [num, setNum] = useState(grid.content);
   const [decimal, setDecimal] = useState(grid.decimal);
   const [percent, setPercent] = useState(grid.percent);
+  const { tableId = "" } = useParams();
   const dispatch = useAppDispatch();
+  const replacedContent = useAppSelector(
+    (state) => state.shouldReplacedContent.shouldReplacedContent[grid._id]
+  );
+
+  useEffect(() => {
+    if (!replacedContent) return;
+    if (replacedContent.type !== TableColumnTypes.Number) {
+      console.error(
+        "CheckboxGrid: replacedContent.type !== TableColumnTypes.Number"
+      );
+      return;
+    }
+    setNum(replacedContent.content);
+    setDecimal(replacedContent.decimal);
+  }, [replacedContent]);
 
   const numStr = getFormattedNumber(num, decimal, percent);
   return isActive ? (
@@ -30,6 +48,15 @@ export const NumberGrid: FC<NumberGridProps> = (props) => {
       defaultValue={numStr}
       onBlur={(e) => {
         setNum(Number(e.target.value));
+        putGridContent({
+          type: TableColumnTypes.Number,
+          content: Number(e.target.value),
+          decimal,
+          percent,
+          gridId: grid._id,
+          rowId,
+          tableId,
+        });
         dispatch(changeActiveGridId(""));
       }}
     />
