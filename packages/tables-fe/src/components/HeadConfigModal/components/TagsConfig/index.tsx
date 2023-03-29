024660c1b24005a7601e844bac9955c281ea86cc
gucan707 <1154984091@qq.com>
2023-03-29 17:42:58 +0800

    feat(fe): 拆分 TagsConfig，增加修改标签ui

---
+++ packages/tables-fe/src/components/HeadConfigModal/components/TagsConfig/index.tsx
@@ -0,0 +1,158 @@
+import "./index.less";
+
+import { FC, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { Button, Input, Tag, Trigger } from "@arco-design/web-react";
+import {
+  MultiSelectHead,
+  SelectHead,
+  SelectOptionType,
+  TableTagColors,
+} from "@tables/types";
+
+import { postTag } from "../../../../http/table/postTag";
+
+type TagsConfigProps = {
+  curHead: SelectHead | MultiSelectHead;
+};
+
+export const TagsConfig: FC<TagsConfigProps> = (props) => {
+  const { curHead } = props;
+  const [newTagColor, setNewTagColor] = useState(TableTagColors.Yellow);
+  const [tagText, setTagText] = useState("");
+  const [curUpdatingTag, setCurUpdatingTag] = useState<
+    SelectOptionType | undefined
+  >();
+  const { tableId = "" } = useParams();
+
+  return (
+    <div className="tags_config-select">
+      <div className="tags_config-select-add">
+        新增标签
+        <Input
+          value={tagText}
+          onChange={(str) => {
+            setTagText(str);
+          }}
+          className="tags_config-select-add-input"
+        />
+        <Trigger
+          popup={() => <TagsColors onTagClick={setNewTagColor} />}
+          trigger="click"
+          position="bottom"
+          classNames="zoomInTop"
+        >
+          <Tag color={newTagColor} className="tags_config-select-add-color">
+            color
+          </Tag>
+        </Trigger>
+        <Button
+          type="text"
+          onClick={() => {
+            postTag({
+              color: newTagColor,
+              headId: curHead._id,
+              text: tagText,
+              tableId,
+            });
+          }}
+        >
+          新增
+        </Button>
+      </div>
+      <div>
+        当前标签：
+        <div className="tags_config-select-tags">
+          {curHead.tags.length
+            ? curHead.tags.map((tag) => (
+                <Tag
+                  bordered={curUpdatingTag?._id === tag._id}
+                  key={tag._id}
+                  color={tag.color}
+                  closable
+                  onClick={() => {
+                    setCurUpdatingTag(tag);
+                  }}
+                >
+                  {tag.text}
+                </Tag>
+              ))
+            : "暂无标签"}
+        </div>
+      </div>
+      {curUpdatingTag && (
+        <div className="tags_config-select-add">
+          修改标签
+          <Input
+            value={curUpdatingTag.text}
+            onChange={(str) => {
+              setCurUpdatingTag({ ...curUpdatingTag, text: str });
+            }}
+            className="tags_config-select-add-input"
+          />
+          <Trigger
+            popup={() => (
+              <TagsColors
+                onTagClick={(color) => {
+                  setCurUpdatingTag({ ...curUpdatingTag, color });
+                }}
+              />
+            )}
+            trigger="click"
+            position="bottom"
+            classNames="zoomInTop"
+          >
+            <Tag
+              color={curUpdatingTag.color}
+              className="tags_config-select-add-color"
+            >
+              color
+            </Tag>
+          </Trigger>
+          <Button
+            type="text"
+            onClick={() => {
+              setCurUpdatingTag(undefined);
+            }}
+          >
+            修改
+          </Button>
+          <Button
+            type="text"
+            onClick={() => {
+              setCurUpdatingTag(undefined);
+            }}
+          >
+            放弃
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+type TagsColors = {
+  onTagClick: (color: TableTagColors) => void;
+};
+
+export const TagsColors: FC<TagsColors> = (props) => {
+  const { onTagClick } = props;
+
+  return (
+    <div className="tags_config-select-add-all_colors">
+      {Object.values(TableTagColors).map((color) => (
+        <Tag
+          key={color}
+          className="tags_config-select-add-all_colors-tag"
+          color={color}
+          onClick={() => {
+            onTagClick(color);
+          }}
+        >
+          {color}
+        </Tag>
+      ))}
+    </div>
+  );
+};
--- packages/tables-fe/src/components/HeadConfigModal/index.tsx
+++ packages/tables-fe/src/components/HeadConfigModal/index.tsx
@@ -1,5 +1,6 @@
 import "./index.less";
 
+import { type } from "os";
 import { FC, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -13,12 +14,16 @@ import {
 } from "@arco-design/web-react";
 import {
   DateFormatOptions,
+  MultiSelectHead,
+  SelectHead,
+  SelectOptionType,
   TableColumnTypes,
   TableTagColors,
 } from "@tables/types";
 
 import { postTag } from "../../http/table/postTag";
 import { useAppSelector } from "../../redux/store";
+import { TagsConfig } from "./components/TagsConfig";
 
 export type HeadConfigModalProps = {
   activeHead: string;
@@ -29,9 +34,6 @@ export const HeadConfigModal: FC<HeadConfigModalProps> = (props) => {
   const { activeHead, setActiveHead } = props;
   const heads = useAppSelector((state) => state.headsReducer.heads);
   const curHead = heads.find((head) => head._id === activeHead);
-  const [newTagColor, setNewTagColor] = useState(TableTagColors.Yellow);
-  const [tagText, setTagText] = useState("");
-  const { tableId = "" } = useParams();
 
   if (!curHead) return null;
 
@@ -56,71 +58,7 @@ export const HeadConfigModal: FC<HeadConfigModalProps> = (props) => {
       );
       break;
     case TableColumnTypes.Select:
-      const colors = (
-        <div className="head_config-select-add-all_colors">
-          {Object.values(TableTagColors).map((color) => (
-            <Tag
-              key={color}
-              className="head_config-select-add-all_colors-tag"
-              color={color}
-              onClick={() => {
-                setNewTagColor(color);
-              }}
-            >
-              {color}
-            </Tag>
-          ))}
-        </div>
-      );
-      content = (
-        <div className="head_config-select">
-          <div className="head_config-select-add">
-            新增标签
-            <Input
-              value={tagText}
-              onChange={(str) => {
-                setTagText(str);
-              }}
-              className="head_config-select-add-input"
-            />
-            <Trigger
-              popup={() => colors}
-              trigger="click"
-              position="bottom"
-              classNames="zoomInTop"
-            >
-              <Tag color={newTagColor} className="head_config-select-add-color">
-                color
-              </Tag>
-            </Trigger>
-            <Button
-              type="text"
-              onClick={() => {
-                postTag({
-                  color: newTagColor,
-                  headId: curHead._id,
-                  text: tagText,
-                  tableId,
-                });
-              }}
-            >
-              新增
-            </Button>
-          </div>
-          <div>
-            当前标签：
-            <div className="head_config-select-tags">
-              {curHead.tags.length
-                ? curHead.tags.map((tag) => (
-                    <Tag key={tag._id} color={tag.color} closable>
-                      {tag.text}
-                    </Tag>
-                  ))
-                : "暂无标签"}
-            </div>
-          </div>
-        </div>
-      );
+      content = <TagsConfig curHead={curHead} />;
       break;
     default:
       content = <>当前类型无额外配置项</>;
@@ -129,8 +67,8 @@ export const HeadConfigModal: FC<HeadConfigModalProps> = (props) => {
 
   return (
     <Modal
-      title="Modal Title"
-      visible={true}
+      title="属性配置"
+      visible
       onOk={() => setActiveHead("")}
       onCancel={() => setActiveHead("")}
       autoFocus={false}
