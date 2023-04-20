cc99561f9e00112476943dd518652094065e4e90
gucan707 <1154984091@qq.com>
2023-04-20 16:10:33 +0800

    feat(be): 所有人都离开时删除被标记为 isDeleted 的数据

--- packages/tables-be/src/socket/leaveRoom.ts
+++ packages/tables-be/src/socket/leaveRoom.ts
@@ -3,6 +3,7 @@ import { Socket } from "socket.io";
 import { Events } from "@tables/types";
 
 import { io } from "../";
+import { rows, tables } from "../db";
 import { roomToUsers } from "./joinRoom";
 
 export async function leaveRoom(socket: Socket) {
@@ -17,9 +18,35 @@ export async function leaveRoom(socket: Socket) {
     (user) => user.socketId !== socket.id
   );
 
+  console.log("roomToUsers[roomId]", roomToUsers[roomId]);
+
   if (!roomToUsers[roomId].length) {
     delete roomToUsers[roomId];
+    console.log("del deleted", roomId);
+    delDeletedColumn(roomId);
   }
 
   io.to(roomId).emit(Events.EmitOnlineUsers, roomToUsers[roomId]);
+}
+
+export async function delDeletedColumn(tableId: string) {
+  const tableInfo = await tables.findOne({ _id: tableId });
+  const deletedHeadIds = tableInfo.heads
+    .filter((head) => head.isDeleted)
+    .map((head) => head._id);
+  tableInfo.heads = tableInfo.heads.filter((h) => !h.isDeleted);
+  await Promise.all([
+    tables.updateOne({ _id: tableId }, { $set: { heads: tableInfo.heads } }),
+    rows.deleteMany({ tableId: tableId, isDeleted: true }),
+    rows.updateMany(
+      { tableId: tableId },
+      {
+        $pull: {
+          data: {
+            headId: { $in: deletedHeadIds },
+          },
+        },
+      }
+    ),
+  ]);
 }
