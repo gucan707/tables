8daf3e4bd1c7c47d32cfc44b40b0c20f036d40b0
gucan <1154984091@qq.com>
2023-05-03 14:16:33 +0800

    fix(fe): 首页头像

--- packages/tables-fe/src/pages/Home/index.tsx
+++ packages/tables-fe/src/pages/Home/index.tsx
@@ -9,6 +9,7 @@ import { ReadonlyTable } from '../../components/ReadonlyTable';
 import { TableCard } from '../../components/TableCard';
 import { createTable } from '../../http/table/createTable';
 import { useTables } from '../../http/table/useTables';
+import { useUserInfo } from '../../http/user/useUserInfo';
 
 const TabPane = Tabs.TabPane;
 
@@ -16,24 +17,27 @@ export const Home: FC = () => {
   let [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams.get("page"));
   const page = Number(searchParams.get("page"));
+  const { userInfo } = useUserInfo();
   const { loading, tables } = useTables({ page: page || 0 });
 
   return (
     <div className="home">
       <header className="home-header">
         <h1>表格总览</h1>
-        <Avatar>A</Avatar>
+        <Avatar>{userInfo?.name}</Avatar>
       </header>
       <div className="home-content">
-        <Button
-          type="primary"
-          style={{ margin: "10px 0" }}
-          onClick={() => {
-            createTable();
-          }}
-        >
-          创建表格
-        </Button>
+        <div>
+          <Button
+            type="primary"
+            style={{ margin: "10px 0" }}
+            onClick={() => {
+              createTable();
+            }}
+          >
+            创建表格
+          </Button>
+        </div>
         <Tabs defaultActiveTab="1">
           <TabPane title="我创建的" key="1">
             <div className="home-content-cards">
